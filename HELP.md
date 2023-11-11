8d55ae8fbcee27fd97159ad5b41838b0ab4a6335
David Gomez <david.gomez@live.com.ar>
2023-11-11 21:38:43 +0000

    first commit

---
+++ HELP.md
@@ -0,0 +1,7 @@
+Project Demo with SpringBoot - Java 17 - Kafka
+
+Source of project:
+https://medium.com/@abhishekranjandev/a-comprehensive-guide-to-integrating-kafka-in-a-spring-boot-application-a4b912aee62e
+
+Base configuration for Docker with Kafka:
+https://www.baeldung.com/ops/kafka-docker-setup
---
+++ src/main/java/com/example/demokafka/controller/KafkaController.java
@@ -0,0 +1,21 @@
+package com.example.demokafka.controller;
+
+import com.example.demokafka.producer.MessageProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+  private final MessageProducer messageProducer;
+
+  public KafkaController(MessageProducer messageProducer) {
+    this.messageProducer = messageProducer;
+  }
+
+  @PostMapping("/send")
+  public String sendMessage(@RequestParam("message") String message) {
+    messageProducer.sendMessage("my-topic", message);
+    return "Message sent: " + message;
+  }
+}
